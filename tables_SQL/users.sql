42fa9276374b75e57af4e1ec820b2b705539a297
Pawday <pawday@mail.ru>
2018-11-11 04:10:09 +0400

    balanCe

--- tables_SQL/users.sql
+++ tables_SQL/users.sql
@@ -4,6 +4,6 @@ create table users
   id_user                 INTEGER(11) not null,
   name                    VARCHAR(50) not null,
   surname                 VARCHAR(80) not null,
-  balanse                 INTEGER(11) not null,
+  balanсe                 INTEGER(11) not null,
   rang                    VARCHAR(20) default 'User' not null
 );
